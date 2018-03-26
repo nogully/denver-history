3fc416cc66d44c507f06fa7a2a5041e0e0669da2
Nora Gully <nora.gully@gmail.com>
2018-03-26 14:40:37 -0600

    Create test environment for travis

--- knexfile.js
+++ knexfile.js
@@ -3,5 +3,10 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/publications',
     useNullAsDefault: true
+  }, 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/denver_history_test',
+    useNullAsDefault: true
   }
 };
