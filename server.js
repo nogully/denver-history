37a3c9caaa99b8e5e5a25ade0ca4869594720e72
Nora Gully <nora.gully@gmail.com>
2018-03-26 14:48:02 -0600

    Create test boilerplate

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.locals.title = 'Denver History';
 
 app.get('/', (request, response) => {
