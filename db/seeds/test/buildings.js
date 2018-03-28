4e481a87632b7e54aaf4f41b35dcf0344a9df81b
Alex Bonder <lexbonder@gmail.com>
2018-03-27 22:28:13 -0600

    Add tests and functionality for GET /api/v1/buildings and /api/v1/buildings/:id

---
+++ db/seeds/test/buildings.js
@@ -0,0 +1,71 @@
+
+exports.seed = (knex, Promise) => {
+  return knex('buildings').del()
+    .then( () => knex('districts').del())
+    .then( () => {
+      return Promise.all([
+        knex('districts').insert([
+          { 
+            id: 0,
+            name: 'Wyman'
+          },
+          {
+            id: 1,
+            name: 'East 7th Avenue'
+          }
+        ], 'id')
+        .then( district => {
+          return knex('buildings').insert([
+            {
+              id: 0,
+              ldmk_num: 93,
+              ldmk_name: '1437 High Street',
+              aka_name: 'Watson House',
+              ord_num: 293,
+              ord_year: 1976,
+              address_line1: '1437 High Street',
+              address_line2: null,
+              situs_num: 1437,
+              situs_dir: null,
+              situs_st: 'High',
+              situs_type: 'ST',
+              historic_dist: district[0],
+              state_hist_num: '5DV.5811',
+              year_built: 1894,
+              arch_bldr: null,
+              document: null,
+              photo_link: 'yes',
+              notes: null,
+              gis_notes: null,
+              description: null,
+              address_id: null
+            },
+            {
+              id: 1,
+              ldmk_num: 94,
+              ldmk_name: 'Kerr House',
+              aka_name: null,
+              ord_num: 360,
+              ord_year: 1976,
+              address_line1: '1900 E. 7th Avenue Parkway',
+              address_line2: null,
+              situs_num: 1900,
+              situs_dir: 'E',
+              situs_st: '7th Avenue',
+              situs_type: 'PKY',
+              historic_dist: district[1],
+              state_hist_num: '5DV.751',
+              year_built: 1925,
+              arch_bldr: null,
+              document: null,
+              photo_link: 'yes',
+              notes: null,
+              gis_notes: null,
+              description: null,
+              address_id: null
+            }
+          ])
+        })
+      ])
+    })
+}
--- server.js
+++ server.js
@@ -3,15 +3,52 @@ const app = express();
 const bodyParser = require('body-parser')
 
 app.set('port', process.env.PORT || 3000);
-
 app.use(bodyParser.json());
-app.use(express.static('public'));
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.locals.title = 'Denver History';
+
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
   response.send('Oh hi');
 });
 
+app.get('/api/v1/buildings', (request, response) => {
+  database('buildings').select()
+    .then( buildings => {
+      response.status(200).json(buildings);
+    })
+    .catch( error => {
+      response.status(500).json({error});
+    })
+});
+
+app.get('/api/v1/buildings/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('buildings').where('id', id).select()
+    .then( result => {
+      if (result.length) {
+        return response
+          .status(200)
+          .json(result)
+      } else {
+        return response
+          .status(404)
+          .send({error: 'That building is not in the database'})
+      }
+    })
+    .catch( error => {
+      response.status(500).json({error})
+    })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
