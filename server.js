3fa94facbd0b250d1d938c12f1666953a116c352
Nora Gully <nora.gully@gmail.com>
2018-03-29 17:37:26 -0600

    Create /search endpoint with query parameter

--- server.js
+++ server.js
@@ -117,6 +117,22 @@ app.delete('/api/v1/districts', checkAuth, (request, response) => {
     .catch(error => response.status(500).send({ error }));
 });
 
+// SEARCH ------------------------------------------
+
+app.get('/api/v1/search', (request, response) => {
+  const key = Object.keys(request.query)[0];
+  const value = Object.values(request.query)[0];
+
+  database('buildings').where(key, value).select()
+    .then(result => {
+      if (result.length) {
+        response.status(200).json(result);
+      } else {
+        response.status(404).json({error: `Property ${key} with ${value} not found.`})
+      }
+    })
+    .catch( error => response.status(500).send({error}) )
+})
 
 // BUILDINGS ---------------------------------------
 
