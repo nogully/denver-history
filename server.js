22b41d1553cc013ba55dd2f54069bffa8ff4bc2c
James Logue <logueja@gmail.com>
2018-04-11 19:58:52 -0600

    Update cors allow for FE dev, add as middleware on endpoints

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ app.locals.title = 'Denver History';
 
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3001'];
+  const origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 // AUTHENTICATION ----------------------
 
 const checkAuth = (request, response, next) => {
