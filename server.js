e43a8541a9370652fbc16ffa519593d27f4c47ef
James Logue <logueja@gmail.com>
2018-04-11 20:06:08 -0600

    Fix lint errors;

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(express.static('public'));
 
 app.use((req, res, next) => {
   const allowedOrigins = ['http://localhost:3001'];
-  const origin = req.headers.origin;
-  if(allowedOrigins.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
+  const { origin } = req.headers;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
